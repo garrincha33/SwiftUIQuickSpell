ff5ee54d8fe912bfdf47c890bcfd8e89869c63c7
Richard Price <richyprice77@gmail.com>
2022-11-16 12:51:45 +0000

    Initial Commit

---
+++ SwiftUIQuickSpell/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUIQuickSpell
+//
+//  Created by Richard Price on 16/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("testing")
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIQuickSpell/SwiftUIQuickSpellApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIQuickSpellApp.swift
+//  SwiftUIQuickSpell
+//
+//  Created by Richard Price on 16/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIQuickSpellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
